c617f700a2a7a8f79ddaa8f521473d6345092129
Christine Kerubo <kerubochri@gmail.com>
2024-07-23 10:15:15 +0300

    Linked list

---
+++ Main.java
@@ -0,0 +1,13 @@
+import java.util.LinkedList;
+
+public class Main{
+    public static void main(String[] args) {
+        LinkedList<String> cars = new LinkedList<String>();
+        cars.add("Volvo");
+        cars.add("Benz");
+        cars.add("Audi");
+        cars.add("Honda");
+
+        System.out.println(cars);
+    }
+}
